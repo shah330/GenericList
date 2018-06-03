fe11930c96e8e082e14e0ec94cfd3151e67d290f
GMRDL7 <gmrstudent@outlook.com>
2018-06-03 11:47:33 -0700

    Adding all files

---
+++ Vedant Shoppping List/Vedant Shoppping List/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vedant_Shoppping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool end = false;
+            int entered = 0;
+            VedantList List = new VedantList();
+
+            while (end == false)
+            {
+                Console.WriteLine("Press 1 to Add, 2 to remove, 3 to view your list, and 4 to end the program");
+                entered = int.Parse(Console.ReadLine());
+                if (entered == 1)
+                {
+                    Console.WriteLine("What do you want to add?");
+                    List.Add(Console.ReadLine());
+                }
+                else if (entered == 2)
+                {
+                    Console.WriteLine("What do you want to remove");
+                    List.Remove(Console.ReadLine());
+                }
+                else if (entered == 3)
+                {
+                    List.Print();
+                }
+                else if(entered == 4)
+                {
+                    end = true;
+                }
+                else
+                {
+                    Console.WriteLine("Try Again");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vedant Shoppping List/Vedant Shoppping List/VedantList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vedant_Shoppping_List
+{
+    class VedantList
+    {
+        string[] list = new string[0];
+
+        public void Add(string item)
+        {
+            string[] tempList = new string[list.Length + 1];
+            for (int i = 0; i < list.Length; i++)
+            {
+                tempList[i] = list[i];
+            }
+            tempList[tempList.Length - 1] = item;
+            list = tempList;
+        }
+
+        public void Remove(string item)
+        {
+            if (list.Length >= 1)
+            {
+                string[] tempList = new string[list.Length];
+                for (int i = 0; i < list.Length; i++)
+                {
+                    if (list[i] != item)
+                    {
+                        tempList[i] = list[i];
+                    }
+                }
+                list = tempList;
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine("{0}", list[i]);
+            }
+        }
+    }
+}
